0360ee94b8caf7f45e6b3278c91a66e55e08691e
peng <peng@bitegginc.com>
2022-01-29 11:09:52 +0700

    PingPong Effect Implemented

--- Assets/Scripts/WallBlock.cs
+++ Assets/Scripts/WallBlock.cs
@@ -12,5 +12,23 @@ public class WallBlock : MonoBehaviour
     {
         rigidbody   = gameObject.GetOrAddComponent<Rigidbody>();
         boxCollider = gameObject.GetOrAddComponent<BoxCollider>();
+
+        rigidbody.useGravity = false;
+        rigidbody.isKinematic = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
     }
+
+    private void OnCollisionEnter(Collision _other)
+    {
+        if(!_other.collider.CompareTag("Ghost")) return;
+
+        var _ghost = _other.collider.GetComponent<Ghost>();
+
+        var _ghostHitDir  = _ghost.GetCurrentDirection();
+        var _hitNormal    = _other.contacts[0].normal;
+        var _reflectDir   = Vector3.Reflect(_ghostHitDir, _hitNormal);
+        
+        _ghost.SetVelocity(new Vector3(_reflectDir.x, 0, _reflectDir.z) * 10f);
+    }
+  
 }
