4696b0cf918d45fb84ecaffe561a317988805fcb
Khami-Desktop <napakamee@outlook.com>
2022-01-29 22:12:41 +0700

    Update multiple camera mask

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,31 +5,33 @@ using UnityEngine.Events;
 
 public class CameraController : MonoBehaviour
 {
+    public LayerMask normalMask;
+    public LayerMask shadowMask;
     public UnityEvent<Filter> OnChangedFilter;
     private Camera camera;
     public void Init()
     {
         camera = Camera.main;
-        HideMask("ShadowRealm");
+        HideMask(normalMask);
         GameManager.Instance.currentFilter = Filter.Normal;
     }
 
 
-    public void ShowMask(string _layerName)
+    public void ShowMask(LayerMask _layerName)
     {
-        camera.cullingMask |= 1 << LayerMask.NameToLayer(_layerName);
+        camera.cullingMask |= 1 << _layerName;
     }
-    public void HideMask(string _layerName)
+    public void HideMask(LayerMask _layerName)
     {
-        camera.cullingMask &=  ~(1 << LayerMask.NameToLayer(_layerName));
+        camera.cullingMask &=  ~(1 << _layerName);
     }
     
     public void SwapFilter()
     {
         GameManager.Instance.currentFilter = GameManager.Instance.currentFilter == Filter.Normal ? Filter.Ghost : Filter.Normal;
         
-        camera.cullingMask ^= 1 << LayerMask.NameToLayer("NormalRealm");
-        camera.cullingMask ^= 1 << LayerMask.NameToLayer("ShadowRealm");
+        camera.cullingMask ^= 1 << normalMask;
+        camera.cullingMask ^= 1 << shadowMask;
         
         OnChangedFilter?.Invoke(GameManager.Instance.currentFilter);
     }
