a7276600e2cb66546ac9428e204800ea7ed9a992
peng <peng@bitegginc.com>
2022-01-29 16:30:10 +0700

    PingPong Finished

--- Assets/Scripts/WallBlock.cs
+++ Assets/Scripts/WallBlock.cs
@@ -32,25 +32,16 @@ public class WallBlock : MonoBehaviour
 
         var _ghost = _other.collider.GetComponent<Ghost>();
 
-        var _ghostHitDir  = _ghost.GetPlaneDirection();
+        var _lastVelocity = _ghost.GetNormalizedLastVelocity();
+        
         var _hitNormal    = _other.contacts[0].normal;
-
         _hitNormal = new Vector3(_hitNormal.x, 0, _hitNormal.z).normalized;
 
-        hitNormal = _hitNormal;
+        hitNormal = -_hitNormal;
 
-        var _reflectDir   = Vector3.Reflect(_ghostHitDir, _hitNormal);
+        var _reflectDir   = Vector3.Reflect(_lastVelocity, -_hitNormal);
         
         _ghost.SetVelocity(_reflectDir * _ghost.Speed);
-    }
-
-    private Vector3 GetReflectVector(Vector3 _hit)
-    {
-        var _hitNormal = new Vector3(_hit.x, 0, _hit.z);
-
-        var _reflectVec = Quaternion.AngleAxis(-20, Vector3.up) * _hitNormal;
-
-        return _reflectVec;
     }
 
     private void OnDrawGizmos()
@@ -58,8 +49,8 @@ public class WallBlock : MonoBehaviour
         Gizmos.color = Color.red;
         if (hitNormal != Vector3.zero)
         {
-            var _target = hitNormal + transform.position;
-            Gizmos.DrawLine(transform.position, _target * 2f);
+            var _target = hitNormal * 2f + transform.position;
+            Gizmos.DrawLine(transform.position, _target);
         }
     }
 }
