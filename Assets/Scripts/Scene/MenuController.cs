bd41cef3a8ef1f43fbb1add39b38c29d57a4ca3c
Khami-Desktop <napakamee@outlook.com>
2022-01-29 17:47:05 +0700

    Add menu controller

--- Assets/Scripts/Scene/MenuController.cs
+++ Assets/Scripts/Scene/MenuController.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject creditCanvas;
+
+    [SerializeField] private string stageSelectName = "02StageSelect";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(stageSelectName);
+    }
     
+    public void ShowCredits()
+    {
+        creditCanvas.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditCanvas.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
