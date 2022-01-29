48f87c2955f77db0fc89dff8a3ee456fa231b000
Khami-Desktop <napakamee@outlook.com>
2022-01-29 23:04:03 +0700

    Update player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,18 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public UnityEvent OnHitDeadZone;
     [SerializeField] private PlayerController playerController;
-
+    
     private void Awake()
     {
         playerController = GetComponent<PlayerController>();
+        OnHitDeadZone.AddListener(playerController.Dizzy);
     }
 
     public PlayerController GetController()
     {
         return playerController;
+    }
+
+    private void OnCollisionEnter(Collision _other)
+    {
+        if (_other.gameObject.CompareTag("DeadZone"))
+        {
+            OnHitDeadZone?.Invoke();
+        }
     }
 }
