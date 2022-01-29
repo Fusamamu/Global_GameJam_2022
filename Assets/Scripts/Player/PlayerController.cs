bb024d893db19644cf16b3d69876e84dcc5dc568
Khami-Desktop <napakamee@outlook.com>
2022-01-29 11:22:48 +0700

    Add player cam

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    private Vector3 moveDirection;
+    [SerializeField] private float inhaleForce = 0.5f;
+    [SerializeField] private float exhaleForce = 0.7f;
+
+    private Transform mousePos;
     private Rigidbody rb;
+    private Camera cam;
+    private LayerMask groundMask;
+    private Vector3 direction;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        groundMask = LayerMask.GetMask("Ground");
+        if (!mousePos)
+        {
+            mousePos = new GameObject("MousePos").transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        var _h = Input.GetAxisRaw("Horizontal");
-        var _v = Input.GetAxisRaw("Vertical");
-        moveDirection = new Vector3(_h, 0f, _v);
-        moveDirection *= speed;
+        Ray _ray = cam.ScreenPointToRay(Input.mousePosition);
 
-        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+        if (Physics.Raycast(_ray, out RaycastHit _hit, float.MaxValue, groundMask))
+        {
+            mousePos.transform.position = _hit.point;
+        }
+
+        var _heading = mousePos.position - transform.position;
+        _heading.y = 0;
+        var _distance = _heading.magnitude;
+        direction = _heading / _distance;
+
+        if (Input.GetMouseButtonUp(0)) //Exhale
+        {
+            rb.AddForce(-direction * inhaleForce, ForceMode.Impulse);
+        }
+        else if (Input.GetMouseButtonUp(1)) //Inhale
+        {
+            rb.AddForce(direction * exhaleForce, ForceMode.Impulse);
+        }
     }
 }
